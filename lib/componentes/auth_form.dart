458051ccec9caf06b6b5864d3ed51c8dd69bd534
andree-felipe <d2022015148@unifei.edu.br>
2024-06-28 16:22:29 -0300

    Implementação da página de loading ao submeter um formulário

--- lib/componentes/auth_form.dart
+++ lib/componentes/auth_form.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import '../models/auth_form_data.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({super.key});
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    super.key,
+    required this.onSubmit,
+  });
 
   @override
   State<AuthForm> createState() => _AuthFormState();
@@ -16,7 +21,8 @@ class _AuthFormState extends State<AuthForm> {
   void _submit() {
     final isValid = _formKey.currentState?.validate() ?? false;
     if(!isValid) return;
-    
+
+    widget.onSubmit(_formData);
   }
 
   @override
