1d225117a1bbb294311fb96c12a63ab96aac97e8
andree-felipe <d2022015148@unifei.edu.br>
2024-06-28 15:52:38 -0300

    Criação das validações dos campos do formulário

--- lib/componentes/auth_form.dart
+++ lib/componentes/auth_form.dart
@@ -14,7 +14,9 @@ class _AuthFormState extends State<AuthForm> {
   final _formData = AuthFormData();
 
   void _submit() {
-    _formKey.currentState?.validate();
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if(!isValid) return;
+    
   }
 
   @override
@@ -33,12 +35,27 @@ class _AuthFormState extends State<AuthForm> {
                   initialValue: _formData.name,
                   onChanged: (name) => _formData.name = name,
                   decoration: const InputDecoration(labelText: 'Nome'),
+                  // ignore: no_leading_underscores_for_local_identifiers
+                  validator: (_name) {
+                    final name = _name ?? '';
+                    if(name.trim().length < 5) {
+                      return 'Nome deve ter no mínimo 5 caracteres';
+                    }
+                    return null;
+                  }
                 ),
               TextFormField(
                 key: const ValueKey('email'),
                 initialValue: _formData.email,
                 onChanged: (email) => _formData.email = email,
                 decoration: const InputDecoration(labelText: 'E-mail'),
+                validator: (_email) {
+                    final email = _email ?? '';
+                    if(!email.contains('@')) {
+                      return 'E-mail informado é inválido';
+                    }
+                    return null;
+                  }
               ),
               TextFormField(
                 key: const ValueKey('password'),
@@ -46,6 +63,13 @@ class _AuthFormState extends State<AuthForm> {
                 onChanged: (password) => _formData.password = password,
                 obscureText: true,
                 decoration: const InputDecoration(labelText: 'Senha'),
+                validator: (_password) {
+                    final password = _password ?? '';
+                    if(password.length < 6) {
+                      return 'Senha deve ter no mínimo 6 caracteres';
+                    }
+                    return null;
+                  }
               ),
               const SizedBox(height: 12),
               ElevatedButton(
